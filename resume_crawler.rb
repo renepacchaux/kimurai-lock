998977222333f749d86751ed8261ba3c4ff85535
Rene M Paccha <paccha@gmail.com>
2021-04-29 20:36:38 -0500

    removed extra

--- resume_crawler.rb
+++
@@ -1,9 +0,0 @@
-describe StringChanger do
-  it 'reverses strings' do
-    string_changer = StringChanger.new
-
-    reversed_string = string_changer.reverse_and_save('example string')
-
-    expect(reversed_string).to eq 'gnirts elpmaxe'
-  end
-end
