72df6b9e86b48d99d36971af0b66f2ade5e5681d
Rene M Paccha <paccha@gmail.com>
2021-07-22 14:37:29 -0500

    updated custom lock

--- crawlers/resume.rb
+++ crawlers/resume.rb
@@ -10,7 +10,8 @@ class Resume < ApplicationCrawler
     # project specific
     "https://renepacchaux-com.webflow.io/case-study/gudeeds-responsive-mobile-app",
     "https://renepacchaux-com.webflow.io/case-study/huntington-bank-pavilion",
-    "https://renepacchaux-com.webflow.io/case-study/this-ux-design-portfolio"
+    "https://renepacchaux-com.webflow.io/case-study/this-ux-design-portfolio",
+    "https://renepacchaux-com.webflow.io/case-study/recipe-revolution"
 
   ]
   @driver = :mechanize
